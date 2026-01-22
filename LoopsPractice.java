98b7f18ad33ff7f1059f6faa3e6957356e0affe3
Abhishek Sharma <abhishek8595731@gmail.com>
2026-01-22 22:09:53 +0530

    Day 3 work

--- LoopsPractice.java
+++ LoopsPractice.java
@@ -17,7 +17,7 @@ public class LoopsPractice {
             for (int i = 1; i <= n; i++) {
             
             for (int j = 1; j <= n; j++) {
-                if (i == 1 || i == n || j == 1 || j == n || j ==n/2) {
+                if (i == 1 || i == n || j == 1 || j == n || j ==n+1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
