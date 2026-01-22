19053b83e931f50ba514ab1ee62307ee8251f6b3
Abhishek Sharma <abhishek8595731@gmail.com>
2026-01-22 19:10:21 +0530

    bug fix

--- LoopsPractice.java
+++ LoopsPractice.java
@@ -1,9 +1,11 @@
 public class LoopsPractice {
-    public static void main(String []args){
-
-for(int i = 1; i<10; i++){
-System.out.println(i);
-
-}
+    public static void main(String[] args) {
+        
+        for(int j = 1; j<= 5; j++){
+        for(int i = 1; i < 5; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+        }
     }
 }
