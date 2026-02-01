0fcba2dc1ecc50dce69c1aece6deb9db35e54561
Abhishek Sharma <abhishek859573@gmail.com>
2026-02-01 14:00:00 +0530

    Notes: More on String methods

---
+++ StringMethods.java
@@ -0,0 +1,12 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        // Method 1 : public char charAt(int index) it gets you the latter on the string that is given by charAtIndex
+
+        String name = "Abhsihek";
+        char latter = name.charAt(0);
+        System.out.println(latter);
+
+
+
+    }
+}
